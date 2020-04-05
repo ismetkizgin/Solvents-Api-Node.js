3cddb6ece3904f0c917bb5f7695c456bfef1f74e
İsmet Kizgin <ismet@brewwww.com>
2020-04-05 17:16:02 +0300

    feat: added cors

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require('dotenv/config');
 const express = require('express');
+var cors = require('cors');
 var bodyParser = require('body-parser');
 
 
+
+// npm i cors --save
+
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
