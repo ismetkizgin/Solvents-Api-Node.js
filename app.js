ea6b01d9badeb90e7f44df7ed1a4613857962f7b
ismet kizgin <info@ismetkizgin.com>
2020-05-21 00:53:00 +0300

    feat: add document page

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ require('dotenv/config');
 const express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname,'document')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +17,7 @@ app.use(require('./router/wordsolver'));
 app.use(require('./router/countdownsolver'));
 
 app.get("/", (req, res, next) => {
-    res.send("OK!");
+    res.sendFile(path.join(__dirname+'/document/index.html'));
 })
 
 app.use((req, res, next) => {
