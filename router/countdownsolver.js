8751750f9e3d26a40c5753a7570ccf7eebbb8bff
ismet kizgin <info@ismetkizgin.com>
2020-08-01 18:16:10 +0300

    fix: fix for routings

--- router/countdownsolver.js
+++ router/countdownsolver.js
@@ -3,9 +3,9 @@ const router = express();
 const countdownsolver = require('../solvent/countdownsolver');
 
 
-router.get("/countdownsolver/:numbers/:target", async (req, res) => {
-    const numbers = req.params.numbers.replace('[', '').replace(']', '').trim().split(',');
-    const target = parseInt(req.params.target.trim());
+router.get("/countdownsolver", async (req, res) => {
+    const numbers = req.body.numbers.replace('[', '').replace(']', '').trim().split(',');
+    const target = parseInt(req.body.target.trim());
     const result = await countdownsolver(numbers.map(Number), target);
     res.send(result);
 });
--- router/wordsolver.js
+++ router/wordsolver.js
@@ -10,8 +10,8 @@ function getTurkishLetter(letter){
     return letter.replace('%C3%96','Ö').replace('%C5%9E','Ş').replace('%C4%9E','Ğ').replace('%C4%B0','İ').replace('%C3%9C','Ü');
 }
 
-router.get("/wordsolver/:letters", async (req, res) => {
-    const letters = req.params.letters.replace('[', '').replace(']', '').trim().split(',');
+router.get("/wordsolver", async (req, res) => {
+    const letters = req.body.letters.replace('[', '').replace(']', '').trim().split(',');
     const word = await wordSolver(letters.map(getTurkishLetter).map(getTrim));
     res.send(word);
 });
