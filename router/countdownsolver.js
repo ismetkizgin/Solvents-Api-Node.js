7e1d4a0bd8eaf8b68b8229a640becd7b095dfddd
ismet kizgin <info@ismetkizgin.com>
2020-08-01 18:24:18 +0300

    feat: feat: added facede pattern in solvent folder

--- router/countdownsolver.js
+++ router/countdownsolver.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express();
-const countdownsolver = require('../solvent/countdownsolver');
-
+const { countdownSolver } = require('../solvent');
 
 router.get("/countdownsolver", async (req, res) => {
     const numbers = req.body.numbers.replace('[', '').replace(']', '').trim().split(',');
     const target = parseInt(req.body.target.trim());
-    const result = await countdownsolver(numbers.map(Number), target);
+    const result = await countdownSolver(numbers.map(Number), target);
     res.send(result);
 });
 
--- router/wordsolver.js
+++ router/wordsolver.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express();
-const wordSolver = require('../solvent/wordsolver');
+const { wordSolver } = require('../solvent');
 
 function getTrim(letter){
     return letter.trim();
