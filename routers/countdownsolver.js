cc4c1b8012259b49d3f4f5e0fd7a196560e5d845
ismet kizgin <info@ismetkizgin.com>
2020-08-02 02:06:27 +0300

    fix: fix for routers

--- routers/countdownsolver.js
+++ routers/countdownsolver.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express();
 const { countdownSolver } = require('../solvent');
 
-router.get("/countdownsolver", async (req, res) => {
+router.post("/countdownsolver", async (req, res) => {
     const numbers = req.body.numbers.replace('[', '').replace(']', '').trim().split(',');
-    const target = parseInt(req.body.target.trim());
+    const target = parseInt(req.body.target);
     const result = await countdownSolver(numbers.map(Number), target);
     res.send(result);
 });
--- routers/wordsolver.js
+++ routers/wordsolver.js
@@ -10,7 +10,7 @@ function getTurkishLetter(letter){
     return letter.replace('%C3%96','Ö').replace('%C5%9E','Ş').replace('%C4%9E','Ğ').replace('%C4%B0','İ').replace('%C3%9C','Ü');
 }
 
-router.get("/wordsolver", async (req, res) => {
+router.post("/wordsolver", async (req, res) => {
     const letters = req.body.letters.replace('[', '').replace(']', '').trim().split(',');
     const word = await wordSolver(letters.map(getTurkishLetter).map(getTrim));
     res.send(word);
